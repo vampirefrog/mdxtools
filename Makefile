eb13530464a3d8bba8f87651d5a266b0bde571a5
vampirefrog <mihai.draghicioiu@gmail.com>
2014-02-13 02:59:43 +0200

    Added vgmtest (generate a test VGM file)

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-programs: mdxstat
+programs: mdxstat vgmtest
 LDFLAGS=-lz
 
 include common.mk
---
+++ vgmtest.cpp
@@ -0,0 +1,7 @@
+#include "VGMWriter.h"
+
+int main(int argc, char **argv) {
+	VGMWriter w;
+	w.write("test.vgm");
+	return 0;
+}
