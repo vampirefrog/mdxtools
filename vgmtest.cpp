41d150501d93d92c148f69e1c88a8746dc0ef62c
Mihai Draghicioiu <mihai.draghicioiu@gmail.com>
2014-02-14 23:01:58 +0200

    improved vgmtest - makes a simple counterpoint with the SN76489

--- vgmtest.cpp
+++ vgmtest.cpp
@@ -4,10 +4,15 @@ int main(int argc, char **argv) {
 	VGMWriter w;
 	w.version = 0x150;
 	w.sn76489_clock = 3579545;
-	w.writePSG(0b10010000); // Volume full
-	w.writePSG(0b10000011);
-	w.writePSG(0b00000000);
-	w.writeWait(44100); // 1 sec
+	w.rate = 60; // japanese
+	float doremi[] = { 261.63, 293.66, 329.63, 349.23, 392.00, 440.00, 493.88, 523.25 };
+
+	for(int i = 0; i < 8; i++) {
+		w.writePSGTone(0, 0, w.sn76489_clock / 32 / doremi[i]);
+		w.writePSGTone(1, 0, w.sn76489_clock / 32 / doremi[7-i/2]);
+		w.writeWait(22050); // 1/2 sec
+	}
+
 	w.write("test.vgm");
 	return 0;
 }
