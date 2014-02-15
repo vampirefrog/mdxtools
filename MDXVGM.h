09bf7bb9643c1878f4d75a668c89969219f45fc1
Mihai Draghicioiu <mihai.draghicioiu@gmail.com>
2014-02-15 02:33:40 +0200

    started MDXVGM

--- MDXVGM.h
+++ MDXVGM.h
@@ -1,8 +1,15 @@
 #ifndef MDXVGM_H_
 #define MDXVGM_H_
 
+#include <map>
+
 class MDXVGM: public MDX {
-	
+	std::map <uint8_t, MDXVoice> voices;
+
+public:
+	virtual void handleVoice(MDXVoice &v) {
+		voices[v.number] = v;
+	}
 };
 
 #endif /* MDXVGM_H_ */
