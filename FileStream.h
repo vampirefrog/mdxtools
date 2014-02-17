9763586ec50a7ef4aeacb69f592f6183d23a7ab3
vampi <mihai.draghicioiu@gmail.com>
2014-02-17 21:38:04 +0200

    FileStream rework. Works on MinGW now.

--- FileStream.h
+++ FileStream.h
@@ -18,28 +18,39 @@
 #include "Buffer.h"
 
 struct FileStream {
-	gzFile f;
+	gzFile zf;
+	FILE *f;
 
 	FileStream() { f = 0; }
-	FileStream(const char *filename, const char *mode="r") {
+	FileStream(const char *filename, const char *mode="rb") {
 		this->open(filename, mode);
 	}
 	~FileStream() {
 		this->close();
 	}
 
-	void open(const char *filename, const char *mode="r") {
-		f = gzopen(filename, mode);
-		if(!f) throw new exceptionf("Could not open %s: %s (%d)", filename, strerror(errno), errno);
+	void open(const char *filename, const char *mode="rb") {
+		if(strchr(mode, 'r')) {
+			f = fopen(filename, mode);
+			if(!f) throw new exceptionf("Could not open %s: %s (%d)", filename, strerror(errno), errno);
+			char head[2];
+			if(fread(head, 1, 2, f) < 2) throw new exceptionf("Could not read signature bytes for %s.", filename);
+			rewind(f);
+			if(head[0] == 0x1f && head[1] == 0x8b) { // detected a gzip file
+				zf = gzdopen(fileno(f), mode);
+				if(!zf) throw new exceptionf("Could not open %s: %s (%d)", filename, strerror(errno), errno);
+			} else zf = 0;
+		} else f = fopen(filename, mode);
 	}
 	void close() {
-		gzclose(f);
+		if(zf) gzclose(zf);
+		fclose(f);
 	}
 	z_off_t tell() {
-		return gztell(f);
+		return zf ? gztell(zf) : ftell(f);
 	}
 	int eof() {
-		return gzeof(f);
+		return zf ? gzeof(zf) : feof(f);
 	}
 
 	bool readCompare(const char *chars, int len = 0) {
@@ -50,10 +61,10 @@ struct FileStream {
 		return ret;
 	}
 	z_off_t seek(z_off_t offset, int whence = SEEK_SET) {
-		return gzseek(f, offset, whence);
+		return zf ? gzseek(zf, offset, whence) : fseek(f, offset, whence);
 	}
 	int read(void *buf, size_t len) {
-		return gzread(f, buf, len);
+		return zf ? gzread(zf, buf, len) : fread(buf, 1, len, f);
 	}
 	uint32_t littleUint32(uint32_t i) {
 		return ((i & 0xff) << 24) | ((i & 0xff00) << 8) | ((i & 0xff0000) >> 8) | ((i & 0xff000000) >> 24);
@@ -63,7 +74,7 @@ struct FileStream {
 	}
 	uint32_t readUint32Big() {
 		uint32_t i;
-		gzread(f, &i, sizeof(i));
+		read(&i, sizeof(i));
 		return ntohl(i);
 	}
 	uint32_t readUint32() {
@@ -71,23 +82,26 @@ struct FileStream {
 	}
 	uint16_t readUint16Big() {
 		uint16_t i;
-		gzread(f, &i, sizeof(i));
+		read(&i, sizeof(i));
 		return ntohs(i);
 	}
 	uint16_t readUint16() {
 		return littleUint16(readUint16Big());
 	}
 	int16_t littleInt16(int16_t i) { return ((i & 0xff) << 8) | ((i & 0xff00) >> 8); }
-	int16_t readInt16Big() { int16_t i; gzread(f, &i, sizeof(i)); return ntohs(i); }
+	int16_t readInt16Big() { int16_t i; read(&i, sizeof(i)); return ntohs(i); }
 	int16_t readInt16() { return littleInt16(readInt16Big()); }
 	uint8_t readUint8() {
-		return gzgetc(f);
+		return getc();
+	}
+	int getc() {
+		return zf ? gzgetc(zf) : fgetc(f);
 	}
 	char *readLine(int stop = '\n') { // TODO: optimize
 		char *s = new char[1];
 		*s = 0;
 		int c, l = 0;
-		while((c = gzgetc(f)) != EOF && c != stop) {
+		while((c = getc()) != EOF && c != stop) {
 			char *ns = new char[l+2];
 			memcpy(ns, s, l);
 			delete[] s;
@@ -110,31 +124,31 @@ struct FileStream {
 		return ret;
 	}
 
-	void write(const char *s, int len=-1) {
-		if(len < 0) len = strlen(s);
-		gzwrite(f, s, len);
+	void write(const void *s, int len=-1) {
+		if(len < 0) len = strlen((char *)s);
+		if(zf) gzwrite(zf, (char *)s, len); else fwrite((char *)s, 1, len, f);
 	}
 
 	void write(Buffer &buf) {
-		gzwrite(f, buf.data, buf.len);
+		write(buf.data, buf.len);
 	}
 
 	void writeUint32(uint32_t i) {
 		uint32_t n = littleUint32(ntohl(i));
-		gzwrite(f, &n, sizeof(n));
+		write(&n, sizeof(n));
 	}
 	void writeUint16(uint16_t i) {
 		uint16_t n = littleUint16(ntohs(i));
-		gzwrite(f, &n, sizeof(n));
+		write(&n, sizeof(n));
 	}
 	void writeUint8(uint8_t i) {
-		gzwrite(f, &i, 1);
+		write(&i, 1);
 	}
 	void fill(size_t size, int c = 0) {
 		unsigned char *buf = (unsigned char *)malloc(size);
 		if(buf) {
 			memset(buf, c, size);
-			gzwrite(f, buf, size);
+			write(buf, size);
 			free(buf);
 		}
 	}
