8ac1a5c74f8b1c26154786809af9773b04a39cfe
vampirefrog <mihai.draghicioiu@gmail.com>
2014-03-03 20:29:52 +0200

    Fixing mdxstat, part I

--- mdxstat.cpp
+++ mdxstat.cpp
@@ -1,43 +1,56 @@
 #include "MDX.h"
 
-class MDXStat: public MDX {
+class MDXStatParser: public MDXChannelParser {
 public:
 	int cmds[32];
+	int notes;
+	int rests;
+	int duration;
+	MDXStatParser(): notes(0), rests(0), duration(0) {
+		memset(cmds, 0, 32 * sizeof(int));
+	}
+
+	virtual void handleRest(uint8_t d) {
+		rests++;
+		duration += d;
+	}
+	virtual void handleNote(uint8_t note, uint8_t d) {
+		notes++;
+		duration += d;
+	}
+	virtual void handleCommand(uint8_t c, ...) {
+		cmds[c - 0xe0]++;
+	}
+};
+
+class MDXStat: public MDX {
+public:
 	int total_cmds[32];
-	int notes, total_notes;
-	int rests, total_rests;
-	int duration, total_duration;
+	int total_notes;
+	int total_rests;
+	int total_duration;
 	MDXStat() {
 		memset(total_cmds, 0, 32 * sizeof(int));
 		total_notes = total_rests = total_duration = 0;
 	}
 	void load(const char *filename) {
-		memset(cmds, 0, 32 * sizeof(int));
-		notes = rests = duration = 0;
-		MDX::load(filename);
-	}
-	virtual void handleRest(uint8_t d) {
-		rests++; total_rests++;
-		total_duration += d;
-		duration += d;
-	}
-	virtual void handleNote(uint8_t note, uint8_t d) {
-		notes++; total_notes++;
-		total_duration += d;
-		duration += d;
-	}
-	virtual void handleCommand(uint8_t c, ...) {
-		cmds[c - 0xe0]++;
-		total_cmds[c - 0xe0]++;
+		MDXStatParser p;
+		MDX::load(filename, p);
+		total_notes += p.notes;
+		total_rests += p.rests;
+		total_duration += p.duration;
+		for(int i = 0; i < 32; i++) {
+			total_cmds[i] += p.cmds[i];
+		}
 	}
 	void outputStats(bool total = false) {
-		int *c = total ? total_cmds: cmds;
+		int *c = total_cmds;
 		printf("notes rests duration ");
 		for(int i = 0; i < 32; i++) {
 			printf("%s%s%02x ", c[i] > 0xff ? " " : "", c[i] > 0xfff ? " " : "", i + 0xe0);
 		}
 		printf("\n");
-		printf(" %04x  %04x   %06x ", total ? total_notes : notes, total ? total_rests : rests, total ? total_duration : duration);
+		printf(" %04x  %04x   %06x ", total_notes, total_rests, total_duration);
 		for(int i = 0; i < 32; i++) {
 			printf("%02x ", c[i]);
 		}
@@ -49,9 +62,13 @@ public:
 int main(int argc, char **argv) {
 	MDXStat mdx;
 	for(int i = 1; i < argc; i++) {
-		mdx.load(argv[i]);
-		printf("%s:\n", argv[i]);
-		mdx.outputStats();
+		try {
+			mdx.load(argv[i]);
+			printf("%s:\n", argv[i]);
+			mdx.outputStats();
+		} catch(exceptionf e) {
+			fprintf(stderr, "Error: %s\n", e.what());
+		}
 	}
 	printf("TOTAL:\n");
 	mdx.outputStats(true);
