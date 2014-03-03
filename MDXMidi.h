ca6a0994d59c172c1de1f45eef926a7fba4e1ecf
vampirefrog <mihai.draghicioiu@gmail.com>
2014-03-03 23:51:20 +0200

    Handle Pan, Volume and mimic some MDXPG behavior

--- MDXMidi.h
+++ MDXMidi.h
@@ -11,10 +11,13 @@ public:
 	MidiWriteChannel mid;
 	uint32_t deltaTime, restTime;
 	uint8_t lastCmd;
+	uint8_t volume, pan;
+
+	MDXMidiChannelParser(): volume(127), pan(3) {}
 
 	virtual void handleNote(uint8_t n, uint8_t duration) {
-		int add = channel >= 8 ? 0 : 27;
-		mid.writeNoteOn(restTime, channel, n + add, 127);
+		int add = channel >= 8 ? 0 : 3;
+		mid.writeNoteOn(restTime, channel, n + add, 100);
 		mid.writeNoteOn(duration, channel, n + add, 0);
 		restTime = 0;
 	}
@@ -33,7 +36,19 @@ public:
 		mid.writeTrackEnd(0);
 	}
 	virtual void handleDetune(int16_t det) {
-		mid.writePitchBend(0, channel, 0x2000 + 0x1000 * (det & 0x3f) / 64);
+		mid.writePitchBend(0, channel, 0x2000 + det * 0xfff);
+	}
+	virtual void handleSetVolume(uint8_t vol) {
+		volume = vol;
+		mid.writeControlChange(0, channel, 7, pan ? MDX::volumeVal(vol) & 0x7f : 0);
+	}
+	virtual void handlePan(uint8_t p) {
+		pan = p;
+		if(pan == 0) {
+			mid.writeControlChange(0, channel, 7, 0);
+		}
+		uint8_t pans[] = { 64, 127, 0, 64 };
+		mid.writeControlChange(0, channel, 10, pans[pan & 0x03]);
 	}
 };
 
@@ -73,11 +88,14 @@ public:
 		o.write("MTrk", 4);
 		o.writeBigUint32(chan.len);
 		o.write(chan);
-		printf("num_channels %d\n", num_channels);
 		for(int i = 0; i < num_channels; i++) {
 			char buf[256];
 			snprintf(buf, sizeof(buf), "Track %d: Channel %c", i+1, channelName(i));
 			parsers[i].mid.writeTrackName(0, buf);
+			parsers[i].mid.writeRPN(0, i, 0, 32); // Pitch Bend Sensitivity
+			parsers[i].mid.writeControlChange(0, i, 13, 0);
+			parsers[i].mid.writeControlChange(0, i, 12, 0);
+			parsers[i].mid.writePitchBend(0, i, 0x2000);
 			while(!parsers[i].ended) parsers[i].feed();
 			parsers[i].mid.writeTrackEnd(0);
 			o.write("MTrk", 4);
