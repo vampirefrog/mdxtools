e369547ab057c4bf6228baaba20ba6ac91f8928a
vampirefrog <mihai.draghicioiu@gmail.com>
2014-03-19 22:38:37 +0200

    mdx2midi cleanup and improvements

--- mdx2midi.cpp
+++ mdx2midi.cpp
@@ -5,35 +5,42 @@
 
 class MDXMidiChannelParser: public MDXMemParser {
 public:
-	MidiWriteChannel mid;
+	MidiWriteTrack mid;
 	uint32_t deltaTime, restTime;
 	uint8_t lastCmd;
 	uint8_t volume, pan;
+	uint8_t firstTempo;
+	uint32_t totalTicks;
 
-	MDXMidiChannelParser(): volume(127), pan(3) {}
+	MDXMidiChannelParser(): volume(127), pan(3), firstTempo(0), totalTicks(0) {}
 
 	virtual void handleNote(uint8_t n, uint8_t duration) {
 		int add = channel >= 8 ? 0 : 3;
 		mid.writeNoteOn(restTime, channel, n + add, 100);
 		mid.writeNoteOn(duration, channel, n + add, 0);
 		restTime = 0;
+		totalTicks += duration;
 	}
 	virtual void handleRest(uint8_t duration) {
 		restTime += duration;
+		totalTicks += duration;
 	}
 	virtual void handleSetVoiceNum(uint8_t voice) {
 		mid.writeProgramChange(0, channel, voice);
 	}
-	virtual void handleSetTempo(uint8_t tempo) {
+	static uint32_t calcTempo(uint32_t tempo) {
 		uint32_t mdxTempo = 1250000 / (256*(256-tempo));
-		uint32_t midiTempo = 60000000 / mdxTempo;
-		mid.writeTempo(0, midiTempo);
+		return 60000000 / mdxTempo;
+	}
+	virtual void handleSetTempo(uint8_t tempo) {
+		mid.writeTempo(0, calcTempo(tempo));
+		if(firstTempo == 0) firstTempo = tempo;
 	}
 	virtual void handleChannelEnd(int chan) {
 		mid.writeTrackEnd(0);
 	}
 	virtual void handleDetune(int16_t det) {
-		mid.writePitchBend(0, channel, 0x2000 + det * 0xfff);
+		mid.writePitchBend(0, channel, 0x2000 + det * 64);
 	}
 	virtual void handleSetVolume(uint8_t vol) {
 		volume = vol;
@@ -57,47 +64,54 @@ public:
 		MDXHeader h;
 		h.read(s);
 		int loopCount = 1;
+		int midiTracks = 1;
+		int firstTempo = 0;
+		uint32_t totalTicks = 0;
 		for(int i = 0; i < h.numChannels; i++) {
 			parsers[i].channel = i;
 			parsers[i].loopIterations = loopCount;
 			parsers[i].dataLen = h.channels[i].length;
 			parsers[i].deltaTime = 0;
-			parsers[i].restTime = 192;
+			parsers[i].restTime = 0;
 			parsers[i].lastCmd = 0;
 			if(parsers[i].dataLen > 0) {
 				parsers[i].data = new uint8_t[parsers[i].dataLen];
 				s.seek(h.fileBase + h.channels[i].offset);
 				s.read(parsers[i].data, parsers[i].dataLen);
+				while(!parsers[i].ended) parsers[i].feed();
+				if(parsers[i].mid.len > 0) {
+					midiTracks++;
+					if(parsers[i].firstTempo > 0 && firstTempo == 0)
+						firstTempo = parsers[i].firstTempo;
+					if(parsers[i].totalTicks > totalTicks) totalTicks = parsers[i].totalTicks;
+				}
 			}
 		}
 
-		FileWriteStream o(outfilename);
-		o.write("MThd", 4);
-		o.writeBigUint32(0x06);
-		o.writeBigUint16(0x01);
-		o.writeBigUint16(h.numChannels);
-		o.writeBigUint16(48);
-		MidiWriteChannel chan;
+		MidiWriteStream o(outfilename);
+		o.writeHeader(midiTracks, 48);
+		MidiWriteTrack firstTrack;
+		firstTrack.writeTempo(0, MDXMidiChannelParser::calcTempo(firstTempo ? firstTempo : 200));
+		firstTrack.writeTimeSignature(0, 4, 2);
 		char buf[256];
 		snprintf(buf, sizeof(buf), "Converted from %s", filename);
-		chan.writeText(0, buf);
-		chan.writeTrackEnd(0);
-		o.write("MTrk", 4);
-		o.writeBigUint32(chan.len);
-		o.write(chan);
+		firstTrack.writeText(0, buf);
+		firstTrack.writeTrackEnd(totalTicks);
+		o.writeTrack(firstTrack);
 		for(int i = 0; i < h.numChannels; i++) {
+			if(parsers[i].mid.len == 0) break;
+			MidiWriteTrack track;
 			char buf[256];
-			snprintf(buf, sizeof(buf), "Track %d: Channel %c", i+1, MDXParser::channelName(i));
-			parsers[i].mid.writeTrackName(0, buf);
-			parsers[i].mid.writeRPN(0, i, 0, 32); // Pitch Bend Sensitivity
-			parsers[i].mid.writeControlChange(0, i, 13, 0);
-			parsers[i].mid.writeControlChange(0, i, 12, 0);
-			parsers[i].mid.writePitchBend(0, i, 0x2000);
-			while(!parsers[i].ended) parsers[i].feed();
-			parsers[i].mid.writeTrackEnd(0);
-			o.write("MTrk", 4);
-			o.writeBigUint32(parsers[i].mid.len);
-			o.write(parsers[i].mid);
+			snprintf(buf, sizeof(buf), "Channel %c (%s)", MDXParser::channelName(i), i > 8 ? "ADPCM" : "FM");
+			track.writeTrackName(0, buf);
+			track.writeBankSelect(0, i, 0);
+			track.writeRPN(0, i, 0, 32); // Pitch Bend Sensitivity
+			track.writeControlChange(0, i, 13, 0);
+			track.writeControlChange(0, i, 12, 0);
+			track.writePitchBend(0, i, 0x2000);
+			track.write(parsers[i].mid);
+			track.writeTrackEnd(0);
+			o.writeTrack(track);
 		}
 	}
 };
