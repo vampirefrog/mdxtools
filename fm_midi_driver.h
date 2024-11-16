64fcca01936de0f85205592dfb971cb8cd9d06d0
vampi <vampi@vampi.tech>
2024-11-17 00:43:27 +0200

    proto fix

--- fm_midi_driver.h
+++ fm_midi_driver.h
@@ -15,7 +15,7 @@ struct fm_midi_driver {
 	struct fm_midi_driver_channel channels[8];
 };
 void fm_midi_driver_init(struct fm_midi_driver *driver, struct midi_file *midi_file);
-void fm_midi_driver_deinit(struct fm_midi_driver *driver);
+void fm_midi_driver_end(struct fm_midi_driver *driver);
 void fm_midi_driver_tick(struct fm_midi_driver *driver);
 
 #endif /* FM_MIDI_DRIVER_H_ */
